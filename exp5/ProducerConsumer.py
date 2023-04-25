2255aff8f4fa9398a4cef930745f5e1e7ed88955
Zane Falcao <falcaozane@gmail.com>
2023-04-25 17:02:55 +0530

    updated exp5

---
+++ exp5/ProducerConsumer.py
@@ -0,0 +1,30 @@
+import threading
+import Queue as queue
+import time
+
+queue = queue.Queue(5)
+
+def pro():
+    global queue
+    while True:
+        if not queue.full():
+            item = time.time()
+            queue.put(item)
+            print("Produced item: ",item)
+            time.sleep(1)
+            
+def cons():
+    global queue
+    while True:
+        if not queue.empty():
+            item = queue.get()
+            print("Consumed item: ",item)
+        time.sleep(2)
+        
+producer_thread = threading.Thread(target=pro)
+consumer_thread = threading.Thread(target=cons)
+
+producer_thread.start()
+consumer_thread.start()
+producer_thread.join()
+consumer_thread.join()
