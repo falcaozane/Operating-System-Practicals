abd6c8da7f4fcfdcb2ba6f10ae495fa9b9febcf0
Zane Vijay Falcao <112317470+falcaozane@users.noreply.github.com>
2023-04-25 13:53:43 +0530

    Create Orphan.c

---
+++ exp3/Orphan.c
@@ -0,0 +1,28 @@
+// C program to demonstrate Orphan process 
+#include<stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+	int pid;
+	pid = fork();
+	
+	if(pid == 0)
+	{
+		printf("I am the child, my process ID is %d\n",getpid());
+		printf("My parent's process ID is %d\n",getppid());
+		sleep(30);
+		printf("\nAfter sleep\nI am the child, my process ID is %d\n",getpid());
+                printf("My parent's process ID is %d\n",getppid());
+		exit(0);
+	}
+	else
+	{
+		sleep(20);
+		printf("I am the parent, my process ID is %d\n",getpid());
+                printf("The parent's parent, process ID is %d\n",getppid());
+		printf("Parent terminates\n");
+	}
+    return 0;
+}
