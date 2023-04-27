4fb2b5483cab44cc2c3e9e1fe4b7bd324b19fe1d
Zane Falcao <falcaozane@gmail.com>
2023-04-27 15:25:04 +0530

    sstf updated

--- exp9/sstf.c
+++ exp9/sstf.c
@@ -1,85 +1,54 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-void main()
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
 {
-    int n;
-    printf("Enter the number of requests\n");
-    scanf("%d",&n);
-    
-    int r[n];
-    printf("Enter the requests one by one\n");
-    for(int i=1; i<=n; i++)
+    int n, head_pos, seek_length = 0, disk_size;
+    printf("Enter the size of the disk: ");
+    scanf("%d", &disk_size);
+    printf("Enter number of requests: ");
+    scanf("%d", &n);
+    int requests[n], done[n];
+    printf("Enter the requests:");
+    for (int i = 0; i < n; i++)
     {
-        scanf("%d",&r[i]);
-    }
-    
-    int h;
-    printf("Enter the head position\n");
-    scanf("%d",&h);
-    
-    
-    int arr1[100];
-    int arr2[100];
-    int count1=0,count2=0;
-    for(int i=1; i<=n; i++)
-    {
-        if(h<=r[i])
+        done[i] = 0;
+        scanf("%d", &requests[i]);
+        if (requests[i] < 0 || requests[i] > disk_size)
         {
-            count1=i-1;
-            count2=i;
-            break;
+            printf("Error: Request is outside the range of the disk %d\n ", i+1);
+            return 0;
         }
     }
-    int k=1;
-    for(int i=count1; i>=1; i--)
-    {
-       arr1[k]=r[i];
-       printf("%d\t",arr1[k]);
-       k++;
+    printf("Enter the head position: ");
+    scanf("%d", &head_pos);
+    if (head_pos < 0 || head_pos > disk_size) {
+    printf("Error: Head position is outside the range of the disk\n");
+    return 0;
     }
-    
-    for(int i=1; i<=count1; i++)
+    printf("Head position: %d\n", head_pos);
+    printf("Sequence: %d -> ", head_pos);
+    for (int count = 0; count < n; count++)
     {
-       r[i]=arr1[i];
-    }
-    
-    printf("\nr array\n");
-    for(int i=1; i<=n; i++)
-    {
-        printf("%d\t",r[i]);
-    }
-    
-    printf("Sequence is: %d",h);
-    for(int i=1; i<=n; i++)
-    {
-        printf("->%d",r[i]);
-    }
-    
-    int soln[n];
-    for(int i=1; i<=n; i++)
-    {
-        if(i==1)
+        int min_dist = disk_size + 1, min_idx = 0;
+        for (int j = 0; j < n; j++)
         {
-            soln[i]=abs(h-r[i]);
+            if (done[j] == 0)
+            {
+                int dist = abs(requests[j] - head_pos);
+                if (dist < min_dist)
+                {
+                    min_dist = dist;
+                    min_idx = j;
+                }
+            }
         }
-        else 
-        {
-            soln[i]=abs(r[i]-r[i-1]);
-        }
-        
+        done[min_idx] = 1;
+        seek_length += min_dist;
+        head_pos = requests[min_idx];
+        printf("%d -> ", requests[min_idx]);
     }
-    
-     printf("\nSoln array is\n");
-    for(int i=1; i<=n; i++)
-    {
-        printf("%d\t",soln[i]);
-    }
-    
-    int st=0;
-    for(int i=1; i<=n; i++)
-    {
-        st=st+soln[i];
-    }
-    printf("\nSeek time is: %d",st);
+    printf("\n");
+    printf("Total seek length: %d\n", seek_length);
+    return 0;
 }
